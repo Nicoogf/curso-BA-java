172e6e7f938bbf0a03c2989799278f2884be1805
Nicolas <nicolasgfalabella@gmail.com>
2025-04-22 19:34:40 -0300

    Clase 01

---
+++ Clase-01/main.java
@@ -0,0 +1,8 @@
+
+//Escribí un programa que imprima una frase diferente a "Hola Mundo", por ejemplo: "¡Bienvenidos al mundo de Java!".
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("¡Bienvenidos al mundo de Java!");
+    }
+}
